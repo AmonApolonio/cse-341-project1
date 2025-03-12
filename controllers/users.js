bc1d0ec36797ce1dd9eb30ec096a17d3543bc9c6
Amon Apolonio <amonapolonio.dev@gmail.com>
2025-03-11 21:43:26 -0300

    fix: changed the user object struture

--- controllers/users.js
+++ controllers/users.js
@@ -21,10 +21,11 @@ const getSingle = async (req, res) => {
 const createUser = async (req, res) => {
     //#swagger.tags = ['Users']
     const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         email: req.body.email,
-        username: req.body.username,
-        name: req.body.name,
-        ipaddress: req.body.ipaddress,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday,
     };
     const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
     if(response.acnowledged) {
@@ -38,12 +39,13 @@ const updateUser = async (req, res) => {
     //#swagger.tags = ['Users']
     const userId = ObjectId.createFromHexString(req.params.id);
     const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         email: req.body.email,
-        username: req.body.username,
-        name: req.body.name,
-        ipaddress: req.body.ipaddress,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday,
     };
-        const response = await mongodb.getDatabase().db().collection('users').replaceOne({_id: userId}, user);
+    const response = await mongodb.getDatabase().db().collection('users').replaceOne({_id: userId}, user);
     if(response.modifiedCount > 0) {
        res.status(200).send();
     }else {
